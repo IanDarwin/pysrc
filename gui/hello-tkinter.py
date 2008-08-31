f32ddfa93824b075f750dff399d6b41ba1ac289c
ian <ian@darwinsys.com>
2008-08-31 18:21:35 +0000

    gui examples

---
+++ gui/hello-tkinter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from Tkinter import Tk,Label
+
+rootWindow = Tk()
+
+label = Label(rootWindow, text="Hello, world!")
+label.pack()
+
+rootWindow.mainloop()
---
+++ gui/tkinter-button.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from Tkinter import Tk,Frame,Button
+
+class HelloButton(object):
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="Exit", command=frame.quit)
+        self.button.pack()
+
+root = Tk()
+
+prog = HelloButton(root)
+
+root.mainloop()
