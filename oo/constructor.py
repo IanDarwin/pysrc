2e37712b2de8a27d5f02e8a56063f0af89dd7d2c
ian <ian@darwinsys.com>
2008-09-11 16:01:55 +0000

    Constructor example

---
+++ oo/constructor.py
@@ -0,0 +1,11 @@
+class BankAccount:
+     def __init__(self, num=0, balance=0.0): # defines constructor
+         self.balance = balance
+     def deposit(self, amount):
+         self.balance += amount
+
+acct=BankAccount(90210) # calls constructor
+print acct.balance
+acct.deposit(2500)
+print acct.balance
+
