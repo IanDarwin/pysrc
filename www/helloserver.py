6b786cdb0e3e4023ead32cd544e298676993bb53
ian <ian@darwinsys.com>
2008-10-28 18:19:54 +0000

    New.

---
+++ www/helloserver.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
+		self.wfile.write(
+			'<h1>Welcome</h1><p>Hello from Python!</p>')
+		return
+        except IOError:
+		self.send_error(500,'Unexpected I/O Error')
+
+def main():
+    try:
+        server = HTTPServer(('', 8080), MyHandler)
+        print 'started httpserver...'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print 'Interrupted, so goodbye!'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
+
