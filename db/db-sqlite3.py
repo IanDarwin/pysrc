4157a5ef356711a96b1c2a6f1b1cd9e2f7764c35
ian <ian@darwinsys.com>
2009-05-25 15:34:09 +0000

    new: sqlite3 example

---
+++ db/db-sqlite3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# all in one - create a table, populate it, query it, drop it.
+
+from sqlite3.dbapi2 import connect
+
+connString = "fred"
+
+conn = connect(connString)
+print conn
+
+cursor = conn.cursor()
+print cursor
+
+cursor.execute("DROP TABLE if exists list")
+
+cursor.execute("""CREATE TABLE list(
+				id integer primary key,
+                  name char(8),
+                  email float)""")
+
+cursor.execute("INSERT INTO list(id,name,email) VALUES(1001, 'Hammy', 'ham@spam.com')")
+cursor.execute("INSERT INTO list(id,name,email) VALUES(1002, 'Spammy', 'spam@ham.com')")
+
+conn.commit()
+
+cursor.execute("select * from list where name like '%y'");
+results = cursor.fetchall()
+for row in results:
+        print "Row:", "id", row[0], \
+			"name", row[1], "email", row[2]
