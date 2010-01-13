7271b112426da052057a457952ac576720a88505
ian <ian@darwinsys.com>
2010-01-13 23:47:22 +0000

    New.

---
+++ oo/bankaccountdemo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# use the BankAccount class in a different file.
+
+from constructor import BankAccount
+
+acct=BankAccount(90210) # calls constructor
+print acct.balance
+acct.deposit(2500)
+print acct.balance
