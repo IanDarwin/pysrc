1a55620973ad20389116d38c9e3373d0f656f265
ian <ian@darwinsys.com>
2010-09-29 14:13:21 +0000

    sync

--- graphics/gnuplotdemo.py
+++ graphics/gnuplotdemo.py
@@ -1,10 +1,12 @@
 import Gnuplot
 
 plotter = Gnuplot.Gnuplot(persist = 1) 
-
 plotter('set data style lines')
 
 # plot x^2 : x for range 0..20
 rd = range(20+1)
 data = [(r,r*r) for r in rd]
+print data
 plotter.plot(data)
+data = [(r,r/4) for f in rd]
+plotter.addplot(data)
