ffaad48db6aff2e02bcf302685effddd3dad1eb7
ian <ian@darwinsys.com>
2010-09-29 14:12:49 +0000

    New: a fake writer?

---
+++ io/fakewriter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# partly-written fake output class. 
+# DO NOT COMMIT until can assign to sys.out and have "print" work.
+
+class fakewriter:
+	buf = ''
+	def write(self, stuff):
+		self.buf += stuff
+	def dump(self):
+		print "Contents of", self, ":"
+		print self.buf
+
+print "Hello"
+out = fakewriter()
+out.write("Hello")
+out.dump()
