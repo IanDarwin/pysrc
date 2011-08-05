7f4dd72a1511493025b5d1fad6c6487333df1670
ian <ian@darwinsys.com>
2011-08-05 17:56:18 +0000

    Make an entry that reports its contents on <Enter> or on button press, to make a more complete example.

--- gui/hello-tkinter.py
+++ gui/hello-tkinter.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
-from Tkinter import Tk,Label
+from Tkinter import Tk,Button,Label,Entry
+
+global field
 
 rootWindow = Tk()
 
 label = Label(rootWindow, text="Hello, world!")
 label.pack()
+
+def handler(*args):
+	global field
+	print "You said", field.get()
+
+field = Entry(rootWindow)
+field.pack()
+field.bind('<Key-Return>', handler)
+
+button = Button(rootWindow, text='Go!', command=handler);
+button.pack()
 
 rootWindow.mainloop()
