192a3444d5fe5a289149ead717df7f21f437a239
Ian Darwin <ian@darwinsys.com>
2019-09-25 11:19:44 -0400

    Import fmt.py from 396 perftest

---
+++ textproc/fmt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+linelength = 72
+out = ""
+
+for line in sys.stdin.readlines():
+	if len(line) == 0:	# null line
+		print ""
+		if out:
+			print out
+			out = ""
+	else:
+		for word in line.split():
+			out += word + ' '
+			if (len(out) > linelength):
+				print out
+				out = ""
+if out:
+	print out
