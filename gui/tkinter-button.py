7af3eb10a6c5c8553b4a6c0ac00473fd8dc6b14b
Ian Darwin <ian@darwinsys.com>
2019-11-28 20:26:02 -0500

    Guido's Indentation Police

--- gui/tkinter-button.py
+++ gui/tkinter-button.py
@@ -3,10 +3,9 @@
 from Tkinter import Tk,Frame,Button
 
 class HelloButton(object):
-    def __init__(self, master):
-
-        frame = Frame(master)
-        frame.pack()
+        def __init__(self, master):
+            frame = Frame(master)
+            frame.pack()
 
         self.button = Button(frame, text="Exit", command=frame.quit)
         self.button.pack()
