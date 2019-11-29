853a89b2fa77f203076db7ea6361737b3db88d7e
Ian Darwin <ian@darwinsys.com>
2019-11-29 08:55:17 -0500

    Another example

---
+++ oo/person.py
@@ -0,0 +1,8 @@
+class Person:
+	''' Defines the information about a person '''
+	def __init__(self, firstname, lastname):
+		self.firstname = firstname;
+		self.lastname = lastname;
+
+	def getName(self):
+		return self.firstname + ' ' + self.lastname;
