270c0433c56214514bc0eba4f8765e9c76a7b1e9
Ian Darwin <ian@darwinsys.com>
2019-11-29 16:53:42 -0500

    Unbreak indentation.

--- gui/tkinter-button.py
+++ gui/tkinter-button.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
+# pip install tkinter if the next line shows 'unresolved':
 from Tkinter import Tk,Frame,Button
 
 class HelloButton(object):
         def __init__(self, master):
             frame = Frame(master)
             frame.pack()
-
-        self.button = Button(frame, text="Exit", command=frame.quit)
-        self.button.pack()
+            self.button = Button(frame, text="Exit", command=frame.quit)
+            self.button.pack()
 
 root = Tk()
 
