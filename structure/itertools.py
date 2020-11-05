2e81587ea37a036790b0f7c92ac26de15b8d1dfb
Ian Darwin <ian@darwinsys.com>
2020-11-05 09:50:55 -0500

    Unbreak and document itertools example

--- structure/itertools.py
+++ structure/itertools.py
@@ -1,6 +1,11 @@
 import itertools
+
 print([x for x in itertools.chain([1, 2, 3], ('a', 'b', 'c'))])
 
-print([x for x in itertools.dropwhile()
+# Iterate over a list, dropping values that are < 0
+print([x for x in itertools.dropwhile(
 	lambda x: x<0, # predicate
-	[-1, -2, -1, 0, 3, 4])]
+	[-1, -2, -1, 0, 3, 4])])
+
+# That one can be done more simply in modern Python with a comprehension though
+print([x for x in [-1, -2, -1, 0, 3, 4] if not x < 0])
