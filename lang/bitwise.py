bddb79db52e2d9b53a211c8e1a9c929fea149b4b
Ian Darwin <ian@darwinsys.com>
2021-04-21 10:31:10 -0400

    Make a bit user-friendlier

--- lang/bitwise.py
+++ lang/bitwise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-""" This is a doc string, taken from the slide in lt1905add.
+""" This entire file (but the last 5 lines) is a doc string, from the slide in lt1905add.
 You can run this as a Python script due to the __main__ at the bottom.
 DocTest checks that your documents pasted from interactive Python still work.
 See https://docs.python.org/3/library/doctest.html
@@ -33,5 +33,7 @@ See https://docs.python.org/3/library/doctest.html
 """
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+	import doctest
+	print("Start tests");
+	doctest.testmod()
+	print("End tests");
