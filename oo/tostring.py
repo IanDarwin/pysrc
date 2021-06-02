a7b22bdc91520418757f8567ab2d9daa86e7874a
Ian Darwin <ian@darwinsys.com>
2021-06-02 14:58:45 -0400

    Better example

--- oo/tostring.py
+++ oo/tostring.py
@@ -3,12 +3,13 @@
 ''' The equivalent of toString in Java and like languages '''
 
 class Trip:
-    def __init__(self, x):
-        self.x = x
+    def __init__(self, depCity, arrCity):
+        self.depCity = depCity
+        self.arrCity = arrCity
         
     def __str__(self):
-        return "Trip:" + self.x
+        return "Trip from {0} to {1}".format(self.depCity, self.arrCity)
 
-trip = Trip('123')
+trip = Trip('Toronto', 'Jamaica')
 print(trip)
 
