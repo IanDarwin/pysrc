3030ac142a35c9acb595711ff790708f1e9389ea
Ian Darwin <ian@darwinsys.com>
2021-06-03 12:12:22 -0400

    'pep8ify'

--- oo/person.py
+++ oo/person.py
@@ -1,11 +1,16 @@
-class Person:
-	''' Defines the information about a person '''
-	def __init__(self, firstname, lastname):
-		self.firstname = firstname;
-		self.lastname = lastname;
+"""
+Defines the information about a person
+"""
 
-	def getName(self):
-		return self.firstname + ' ' + self.lastname;
+
+class Person:
+	def __init__(self, firstname, lastname):
+		self.firstname = firstname
+		self.lastname = lastname
+
+	def get_name(self):
+		return self.firstname + ' ' + self.lastname
+
 
 ian = Person('Ian', 'Darwin')
-print(ian)
+print(ian.get_name())
