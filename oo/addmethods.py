dd805b5dcfdf09a44639e4114512ca3d2da41a19
Ian Darwin <ian@darwinsys.com>
2021-12-21 16:34:09 -0500

    Add methods both to instance and to class

--- oo/addmethods.py
+++ oo/addmethods.py
@@ -5,10 +5,13 @@ class X:
 	def __init__(self, foo = 0):
 		self.foo = foo
 
-x = X()
-setattr(x, 'bar', lambda: x.foo + 1)
-print(x.bar())
+# Add a method to one instance:
+x1 = X()
+setattr(x1, 'bar', lambda: x1.foo + 1)
+print(x1.bar())
 
-x = X(42)
-setattr(x, 'bar', lambda: x.foo + 1)
-print(x.bar())
+# Add the same method to the class, so all
+# instances will have it.
+X.bar = lambda x: x.foo + 1
+x2 = X(42)
+print(x2.bar())
