cbf33be52bf195596007967d1fc4064c01310b8d
Ian Darwin <ian@darwinsys.com>
2021-12-21 20:23:21 -0500

    never-concatenate loops, gives warnings

--- db/never-concatenate.py
+++ db/never-concatenate.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 # If you don't know why this is here, see https://xkcd.com/327/
+# Meanwhile, DO NOT DO THIS in production!
 
 sql_template = "select * from students where name = '{}'"
-name = input("Enter student login name: ")
-sql_command = sql_template.format(name);
-print(sql_command)
+
+while True:
+	name = input("Enter student login name: ")
+	sql_command = sql_template.format(name);
+	print(sql_command)
+	if sql_command.find("';") > 0:
+		print("Looks like you mighta got pwned, pardner!");
+	else:
+		print("What a nice name!");
