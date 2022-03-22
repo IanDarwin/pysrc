c56523fd1c9a79f9c43ddad46d601107f0dcc052
Ian Darwin <ian@darwinsys.com>
2022-03-22 13:38:22 -0400

    Note on static methods

--- oo/statics.py
+++ oo/statics.py
@@ -1,4 +1,9 @@
-# A class variable and an instance variable of the same name are distinct!
+# A 'class attribute' is like a static method in C-family (Java, C++, C#, etc)
+
+# A class attribute and an instance variable of the same name are distinct!
+
+# For class methods (which are subtly different from static methods), see
+# https://www.geeksforgeeks.org/class-method-vs-static-method-python/
 
 class One:
 	name = "Kellogs"
@@ -8,3 +13,4 @@ o.name = "Post"
 
 print("o.name = " + o.name)
 print("One.name = " + One.name)
+
