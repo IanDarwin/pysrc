18924086bfb63e9991bceab8dd1c7cfcd8764a62
Ian Darwin <ian@darwinsys.com>
2022-03-24 15:23:11 -0400

    Kill awful formatting; add constr param

--- oo/hasattr.py
+++ oo/hasattr.py
@@ -1,9 +1,12 @@
 class A:
-	classdata = 15
-	def __init__(self): self.data = 22
-	def display(self): print(self.data)
+	classdata = "some class stuff here"
+	def __init__(self, data):
+		self.data = data
+	def display(self):
+		print(self.data)
 
-a = A()
+a = A(22)
 print(dir(a))
 print(hasattr(a, 'classdata'), hasattr(a, 'display'), hasattr(a, '__del__'))
+a.display()
 
