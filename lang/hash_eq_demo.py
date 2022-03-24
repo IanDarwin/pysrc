3a71a926ec3a0dfa6b4a9710f214236276b04ad5
Ian Darwin <ian@darwinsys.com>
2022-03-24 12:44:54 -0400

    Don't mix spaces & tabsi, even in the same file

--- lang/hash_eq_demo.py
+++ lang/hash_eq_demo.py
@@ -3,30 +3,30 @@ Attempts to implement these definitions from Python documentation:
 
 hashable
 
-    An object is hashable if it has a hash value which never changes
-    during its lifetime (it needs a __hash__() method), ...
+	An object is hashable if it has a hash value which never changes
+	during its lifetime (it needs a __hash__() method), ...
 	[Note that this is different from Java, and probably C#]
 	... and can be
-    compared to other objects (it needs an __eq__() or __cmp__()
-    method). Hashable objects which compare equal must have the same
-    hash value.
+	compared to other objects (it needs an __eq__() or __cmp__()
+	method). Hashable objects which compare equal must have the same
+	hash value.
 
-    Hashability makes an object usable as a dictionary key and a set member,
+	Hashability makes an object usable as a dictionary key and a set member,
 	because these data structures use the hash value internally.
 
-    All of Python's immutable built-in objects are hashable, while no
-    mutable containers (such as lists or dictionaries) are. Objects
-    which are instances of user-defined classes are hashable by
-    default; they all compare unequal (except with themselves), and
-    their hash value is derived from their id().
+	All of Python's immutable built-in objects are hashable, while no
+	mutable containers (such as lists or dictionaries) are. Objects
+	which are instances of user-defined classes are hashable by
+	default; they all compare unequal (except with themselves), and
+	their hash value is derived from their id().
 
 immutable
 
-    An object with a fixed value. Immutable objects include numbers,
-    strings and tuples. Such an object cannot be altered. A new object
-    has to be created if a different value has to be stored. They play
-    an important role in places where a constant hash value is needed,
-    for example as a key in a dictionary.
+	An object with a fixed value. Immutable objects include numbers,
+	strings and tuples. Such an object cannot be altered. A new object
+	has to be created if a different value has to be stored. They play
+	an important role in places where a constant hash value is needed,
+	for example as a key in a dictionary.
 
 By those definitions, this class is hashable but NOT immutable.
 Could make it the latter by renaming name to __name throughout.
