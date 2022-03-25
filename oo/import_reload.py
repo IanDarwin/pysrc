577f659dedd7ba6eed14b8fe4f830e5ccd490941
Ian Darwin <ian@darwinsys.com>
2022-03-25 10:16:54 -0400

    Simplify

--- oo/import_reload.py
+++ oo/import_reload.py
@@ -1,6 +1,5 @@
 '''
 Show how to reload an imported module.
-Don't ask me why it's so verbose - used to be a builtin.
 '''
 def dump():
 	print(dir(math))
@@ -10,8 +9,8 @@ import math
 dump()
 
 # Re-load the math module
-import sys, importlib
-importlib.reload(sys.modules['math'])
+import importlib
+importlib.reload(math)
 dump();
 
 # The cheesy way (may be less efficient; gets a new id()
