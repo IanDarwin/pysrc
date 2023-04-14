12073b788cd687b553bcb348e6f52a3ca2544e05
Ian Darwin <ian@darwinsys.com>
2023-04-14 19:58:48 -0400

    Better example

--- io-pathlib/write_read.py
+++ io-pathlib/write_read.py
@@ -6,10 +6,16 @@ from pathlib import Path
 
 pt = Path("/tmp/ian.adoc")
 
+print("Text stuff");
 pt.write_text("= My Exciting Article About Pathlib\n");
 with pt.open(mode="a") as file:
-    file.write(":author: Ian Darwin\n")
+	file.write(":author: Ian Darwin\n")
+	file.write("\n");
+	file.write('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt')
 print(pt.read_text())
+
+print();
+print("Binary stuff");
 
 pb = Path("/tmp/ian.bin")
 pb.write_bytes(b'\1\2\3\4\5')
