8af34989c84828b48fba692bed388290703f8a2e
Ian Darwin <ian@darwinsys.com>
2023-05-03 17:46:38 -0400

    move doc-comment to function

--- oo/tostring.py
+++ oo/tostring.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-''' The equivalent of toString in Java and like languages '''
 
 class Trip:
     def __init__(self, depCity, arrCity):
         self.depCity = depCity
         self.arrCity = arrCity
         
+    ''' The equivalent of toString in Java and like languages '''
     def __str__(self):
         return "Trip from {} to {}".format(self.depCity, self.arrCity)
 
