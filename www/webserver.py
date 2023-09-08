eb0bd12c6b97b4a6428f795c334b71442cc002f3
Ian Darwin <ian@darwinsys.com>
2023-09-08 08:48:38 -0400

    WIP to repair indentation

--- www/webserver.py
+++ www/webserver.py
@@ -9,35 +9,32 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-	print("doGet: path==" + self.path)
+	    print("doGet: path==" + self.path)
         try:
-	    if self.path == "/":
-		self.path = "index.html"
-            if self.path.endswith(".html"):
-                f = open(curdir + sep + self.path) #self.path has /test.html
+            if self.path == "/":
+                self.path = "index.html"
+                if self.path.endswith(".html"):
+                    f = open(curdir + sep + self.path) #self.path has /test.html
 
 # note that the above line potentially makes every file on your computer readable by the internet
 
-                self.send_response(200)
-                self.send_header('Content-type',	'text/html')
-                self.end_headers()
-                self.wfile.write(f.read())
-                f.close()
-                return
-            if self.path.endswith(".esp"):   #our dynamic content
-                self.send_response(200)
-                self.send_header('Content-type',	'text/html')
-                self.end_headers()
-                self.wfile.write("hey, today is the " + str(time.localtime()[7]))
-                self.wfile.write(" day in the year " + str(time.localtime()[0]))
-                return
-                
+                    self.send_response(200)
+                    self.send_header('Content-type',    'text/html')
+                    self.end_headers()
+                    self.wfile.write(f.read())
+                    f.close()
+                    return
+                if self.path.endswith(".esp"):   #our dynamic content
+                    self.send_response(200)
+                    self.send_header('Content-type',    'text/html')
+                    self.end_headers()
+                    self.wfile.write("hey, today is the " + str(time.localtime()[7]))
+                    self.wfile.write(" day in the year " + str(time.localtime()[0]))
+                    return
             return
-                
         except IOError:
             self.send_error(404,'File Not Found: %s' % self.path)
      
-
     def do_POST(self):
         global rootnode
         try:
@@ -49,7 +46,7 @@ class MyHandler(BaseHTTPRequestHandler):
             self.end_headers()
             upfilecontent = query.get('upfile')
             print("filecontent", upfilecontent[0])
-			# Note that for this demo, for security, we don't actually save the file.
+            # Note that for this demo, for security, we don't actually save the file.
             self.wfile.write("<HTML>POST OK.<BR><BR>");
             self.wfile.write(upfilecontent[0]);
             
