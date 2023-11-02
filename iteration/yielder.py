89d082529f999879b7a41f86396d1158030765b3
Ian Darwin <ian@darwinsys.com>
2023-11-02 12:06:29 -0400

    Expand demo

--- iteration/yielder.py
+++ iteration/yielder.py
@@ -8,12 +8,14 @@ def squareList(l):
 iter = squareList(range(1,5))
 print("Iter = ", iter)
 
+print("From next()")
 print("iter next() = ", next(iter))
 print("iter.next() = ", next(iter))
 
-print("Resetting generator")
-iter = squareList(range(1,5))
+# print("Resetting generator")
+# iter = squareList(range(1,5))
 
 # Iteration (normal):
+print("From for-loop")
 for s in iter:
 	print(s)
