cd0f17c4604bd0e2927b12d2bd1cd44c87b21330
Ian Darwin <ian@darwinsys.com>
2024-03-21 15:12:16 -0400

    Add docstring

--- oo/tostring.py
+++ oo/tostring.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-
 class Trip:
-    def __init__(self, depCity, arrCity):
-        self.depCity = depCity
-        self.arrCity = arrCity
-        
-    ''' The equivalent of toString in Java and like languages '''
-    def __str__(self):
-        return "Trip from {} to {}".format(self.depCity, self.arrCity)
+	"Class represents a Trip from one city to another."
+	def __init__(self, depCity, arrCity):
+		self.depCity = depCity
+		self.arrCity = arrCity
+		print(self.__doc__)
+		
+	''' The equivalent of toString in Java and like languages '''
+	def __str__(self):
+		return "Trip from {} to {}".format(self.depCity, self.arrCity)
 
 trip = Trip('Toronto', 'Jamaica')
 
