c72c5a561c7c6d720be2beb76ab1d1d2280d0ff3
Ian Darwin <ian@darwinsys.com>
2024-09-04 21:09:30 -0400

    add dont_modify_while_looping.py

---
+++ iteration/dont_modify_while_looping.py
@@ -0,0 +1,11 @@
+def crashAndBurn():
+	nums=list(range(5))
+	for num in nums:
+		print(num)
+		nums.insert(0,'x')
+
+if __name__ == '__main__':
+	ans = input("Are you sure you want to loop forever?")
+	if len(ans) > 0 and ans.lower()[0] == 'y':
+		input("OK, it's your funeral. Hit <return> to start. Use CTRL/C to stop");
+		crashAndBurn();
