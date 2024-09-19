b5e689cfd9697fd2bc59be6252ab20703f1c037a
Ian Darwin <ian@darwinsys.com>
2024-09-19 14:38:21 -0400

    PEP8ify

--- oo/tostring.py
+++ oo/tostring.py
@@ -4,17 +4,22 @@
 # so you may want to use a __str__ function instead.
 
 class Trip:
-	"Class represents a Trip from one city to another."
-	def __init__(self, depCity, arrCity):
-		self.depCity = depCity
-		self.arrCity = arrCity
-		print(self.__doc__)
-		
+	"""The Trip class represents one journey from one city to another."""
+	def __init__(self, dep_city, arrival_city):
+		self.dep_city = dep_city
+		self.arrival_city = arrival_city
+
 	''' The equivalent of toString in Java and like languages '''
 	def __str__(self):
-		return f"Trip from {self.depCity} to {self.arrCity}"
+		return f"Trip from {self.dep_city} to {self.arrival_city}"
+
+	# Not customary! Just to show the __doc__ attribute
+	def help(self):
+		print(self.__doc__)
 
 trip = Trip('Toronto', 'Jamaica')
 
 print(trip)
+
+trip.help();
 
