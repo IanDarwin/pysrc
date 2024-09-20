1cae4d6ddb5408d266dd6c30ea1999c58e65c28d
Ian Darwin <ian@darwinsys.com>
2024-09-20 08:13:59 -0400

    Better name. Better comments.

--- structure/itertoolsdemo.py
+++ structure/itertoolsdemo.py
@@ -1,5 +1,7 @@
 import itertools
 
+# Simply chain together non-addable collections
+# [...] + (...) gives a TypeError
 print([x for x in itertools.chain([1, 2, 3], ('a', 'b', 'c'))])
 
 # Iterate over a list, dropping values that are < 0
@@ -7,5 +9,5 @@ print([x for x in itertools.dropwhile(
 	lambda x: x<0, # predicate
 	[-1, -2, -1, 0, 3, 4])])
 
-# That one can be done more simply in modern Python with a comprehension though
+# The latter can be done more simply with a comprehension though
 print([x for x in [-1, -2, -1, 0, 3, 4] if not x < 0])
