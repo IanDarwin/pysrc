38abeec06355fcb7e9833c35e1719a67fc2a77e3
Ian Darwin <ian@darwinsys.com>
2025-07-06 16:58:25 -0400

    Add decorator-based version

--- oo/accessors.py
+++ oo/accessors.py
@@ -2,38 +2,51 @@
 Adapted from https://docs.python.org/3/library/functions.html#property
 """
 class Accessors:
-    def __init__(self):
-        self._x = None
-        self._meaning = 42
+	def __init__(self):
+		self._x = None
+		self._meaning = 42
+		self._name = "Monty"
 
-    def getx(self):
-        print("In getx()")
-        return self._x
+	def getx(self):
+		print("In getx()")
+		return self._x
 
-    def setx(self, value):
-        print("In setx()")
-        self._x = value
+	def setx(self, value):
+		print("In setx()")
+		self._x = value
 
-    def delx(self):
-        print("In delx()")
-        del self._x
+	def delx(self):
+		print("In delx()")
+		del self._x
 
-    x = property(getx, setx, delx, "I'm the 'x' property.")
+	# Property can be defined explicitly:
+	x = property(getx, setx, delx, "I'm the 'x' property.")
 
-    @property
-    def meaning(self):
-        """Get the meaning of life"""
-        return self._meaning
+	def meaning(self):
+		"""Get the meaning of life"""
+		return self._meaning
+
+	# Or can be defined using decorators:
+	@property
+	def name(self):
+		return self._name;
+
+	@name.setter
+	def name(self, name):
+		self._name = name;
 
 def main():
-    print("Starting")
-    obj = Accessors()
-    print(obj.x)        # Invokes get method
-    obj.x = 42          # Invokes set method
-    print(obj.x)        # Invokes get method
-    del obj.x           # Invokes del method
-    print(dir(obj))
-    print("Finally, the meaning of life is...", obj.meaning)
+	print("Starting")
+	obj = Accessors()
+	print(obj.x)		# Invokes get method
+	obj.x = 42		  # Invokes set method
+	print(obj.x)		# Invokes get method
+	del obj.x		   # Invokes del method
+	print(dir(obj))
+	print("Finally, the meaning of life is...", obj.meaning())
+	print(obj.name);
+	obj.name = "Brian"
+	print(obj.name);
 
 if __name__ == '__main__':
-    main()
+	main()
