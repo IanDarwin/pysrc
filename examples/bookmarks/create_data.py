9ed90f8715a949515cd0d9f544543b1e427086c8
Ian Darwin <ian@darwinsys.com>
2025-07-08 19:30:43 -0400

    Python script to get URLs from teachnotes and load into SQL DB.

---
+++ examples/bookmarks/create_data.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# create and populate the database.
+
+# for SQLite
+from sqlite3.dbapi2 import connect
+connString = "bookmarks.db"
+
+sql_create_topics = "CREATE TABLE topics(id character varying(32) not null, description character varying);"
+
+sql_create_bookmarks = """
+	CREATE TABLE bookmarks(
+		id bigint integer primary key,
+		owner integer default 1,
+		url character varying not null,
+		text character varying not null,
+		topic_id character varying not null references topic(id)
+	);"""
+
+sql_topic = 'python' # used as pkey and fkey
+
+with connect(connString) as conn:
+	cur = conn.cursor();
+
+	cur.execute(sql_create_topics)
+	cur.execute(f"INSERT INTO topics(id,description) VALUES('{sql_topic}', 'Python Language and Applications')")
+	cur.execute(sql_create_bookmarks)
+
+	with open("/home/ian/lt1906/teachnotes.txt") as ifile:
+		for line in ifile:
+			if line.startswith("https:"):
+				s = line[:-1].split(' ', 1);
+				if len(s) != 2:
+					raise Exception("Wrong number of fields on " + line);
+				url = s[0]
+				text = s[1]
+				s = f"INSERT INTO bookmarks(topic_id, url, text) VALUES('{sql_topic}', '{url}', '{text}');"
+				cur.execute(s);
+print("Insertions All Done");
