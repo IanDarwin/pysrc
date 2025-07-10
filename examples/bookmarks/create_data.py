7f0630925f962a0c0b6e2ffbef80a4aacefcae85
Ian Darwin <ian@darwinsys.com>
2025-07-10 10:07:23 -0400

    Tweak SQL to avoid duplicates

--- examples/bookmarks/create_data.py
+++ examples/bookmarks/create_data.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 # create and populate the database.
+# The SQL is complicated by the need to be idempotent since
+# we update the text file from time to time and want to
+# "sync" that into the database when it changes.
 
 # for SQLite
 from sqlite3.dbapi2 import connect
@@ -10,8 +13,12 @@ connString = "bookmarks.db"
 # from psycopg2 import connect
 # connString = "dbname=test user=test password=fred host=server"
 
-sql_create_topics = "CREATE TABLE IF NOT EXISTS topics(id character varying(32) not null, description character varying);"
+# input file
+filename = "/home/ian/lt1906/teachnotes.txt"
 
+# Table creation
+sql_create_topics = ("CREATE TABLE IF NOT EXISTS topic("
+		"id character varying(32) not null unique, description character varying);")
 sql_create_bookmarks = """
 	CREATE TABLE IF NOT EXISTS bookmark(
 		id bigint integer primary key,
@@ -21,16 +28,16 @@ sql_create_bookmarks = """
 		topic_id character varying not null references topic(id)
 	);"""
 
-sql_topic = 'py' # used as pkey and fkey
+sql_topic = 'py' # used as pkey in topics, and fkey in bookmark
 
 with connect(connString) as conn:
-	cur = conn.cursor();
+	cur = conn.cursor()
 
 	cur.execute(sql_create_topics)
-	cur.execute(f"INSERT INTO topics(id,description) VALUES('{sql_topic}', 'Python Language and Applications')")
+	cur.execute(f"INSERT INTO topic(id,description) VALUES('{sql_topic}',"
+				f"'Python Language, Frameworks, and Applications') on conflict do nothing;")
 	cur.execute(sql_create_bookmarks)
 
-	filename = "/home/ian/lt1906/teachnotes.txt"
 	try:
 		ifile = open(filename)
 	except FileNotFoundError:
