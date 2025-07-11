a7b487975a79ec5b76daf7b64108a7ea9c5c42ca
Ian Darwin <ian@darwinsys.com>
2025-07-11 14:28:35 -0400

    Another example

--- structure/itertoolsdemo.py
+++ structure/itertoolsdemo.py
@@ -11,3 +11,10 @@ print([x for x in itertools.dropwhile(
 
 # The latter can be done more simply with a comprehension though
 print([x for x in [-1, -2, -1, 0, 3, 4] if not x < 0])
+
+# Classic "combinations" demo
+sauces = ['cubano', 'tobasco', 'hot sauce', 'salsa', 'mushroom', 'gravy']
+num_toppings = 3
+print(f"Combination of {num_toppings} sauces on a noodle dish")
+for tops in itertools.combinations(sauces, num_toppings):
+    print('Pasta with', tops)
