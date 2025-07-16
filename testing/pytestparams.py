5b1e9dad8520e2fab4b40469365d7bd087b50591
Ian Darwin <ian@darwinsys.com>
2025-07-15 21:01:47 -0400

    Typo in prev

--- testing/pytestparams.py
+++ testing/pytestparams.py
@@ -2,9 +2,9 @@
 
 from pytest import mark
 
-@mark.parametrize("one,two,expected")[
+@mark.parametrize("one,two,expected",[
 	(2,2,4),
 	(3,4,7)
-]
+])
 def testAdd(one,two,expected):
-	assert one + 2 == expected
+	assert one + two == expected
