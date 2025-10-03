49610f943fc8c61d80385a26c2561fdf9186c015
Ian Darwin <ian@darwinsys.com>
2025-10-03 11:44:35 -0400

    Tweak wording

--- lang/exceptions.py
+++ lang/exceptions.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-# demonstrate Python's variant of try/catch/finally
+# demonstrate Python's try/except/finally (like Java's try/catch/finally)
 
 try:
 	print("Trying")
 	raise IndexError
-	print("Should not get here!")
+	print("Should not get here!") # Causes compiler warning
 except IndexError:
 	print("Caught expected IndexError:")
 except "Erised":
-	print("Strange Error: Caught Erised: should not happen.")
+	print("Magical Error: Caught str 'Erised': should not happen.")
 finally:
 	print("In finally, Doing boring clean-up")
 
